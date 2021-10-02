cdd52400a84d740f8d20ada80bcc6f9279f43513
Giorgos-vr <giorgos.vr@gmail.com>
2021-10-02 16:29:54 +0300

    new projects

--- orbits/main.py
+++ orbits/main.py
@@ -1,2 +1,22 @@
-
 import pygame
+import os
+
+
+width = 1280
+height = 720
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Orbital Resonance V0.1")
+
+
+
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
