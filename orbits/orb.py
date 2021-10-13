499c2f8cce2482eb2c5410a6303012eee7bed912
Giorgos-vr <89841637+Giorgos-vr@users.noreply.github.com>
2021-10-13 23:46:06 +0300

    functional progamming y'all!

--- orbits/orb.py
+++ orbits/orb.py
@@ -25,11 +25,11 @@ distance2 = 125
 planet2_orbit = 0
 angle2_inc = .04
 size2 = 10
-distance3 = 200
+distance3 = 225
 planet3_orbit = 0
 angle3_inc = .02
 size3 = 8
-distance4 = 275
+distance4 = 325
 planet4_orbit = 0
 angle4_inc = .01
 size4 = 12
@@ -42,14 +42,15 @@ sat_orb2 = 0
 sat_angle_inc2 = .03
 sat_size2 = 5
 
-class planet_move:
-    def planet_coors(planet_orbit, distance, angle_inc):
+
+class PlanetMove:
+    def planet_coors(planet_orbit, distance):
 
         planet_x = math.cos(planet_orbit) * distance + 640
         planet_y = -math.sin(planet_orbit) * distance + 360
-        planet_orbit += angle_inc
         return planet_x, planet_y
     
+
 stars = [(random.randint(0, 1279), random.randint(0, 719)) for x in range(140)]
 clock = pygame.time.Clock()
 run = True
@@ -60,18 +61,15 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-    
-    planet_move.planet_coors(planet1_orbit, distance1, angle1_inc)
-    planet_move.planet_coors(planet2_orbit, distance2, angle2_inc)
-    planet_move.planet_coors(planet3_orbit, distance3, angle3_inc)
-    planet_move.planet_coors(planet4_orbit, distance4, angle4_inc)
-
+    PlanetMove.planet_coors(planet1_orbit, distance1)
     planet1_orbit += angle1_inc
+    PlanetMove.planet_coors(planet2_orbit, distance2)
     planet2_orbit += angle2_inc
+    PlanetMove.planet_coors(planet3_orbit, distance3)
     planet3_orbit += angle3_inc
+    PlanetMove.planet_coors(planet4_orbit, distance4)
     planet4_orbit += angle4_inc
-    sat_orb1 += sat_angle_inc1
-    sat_orb2 += sat_angle_inc2
+
 
     screen.fill(black)
 
@@ -80,14 +78,11 @@ while run:
         pygame.draw.line(screen, white, (x, y), (x, y))
 
     pygame.draw.circle(screen, white, center, star_radius)
-    pygame.draw.circle(screen, yellow, (planet_move.planet_coors(planet1_orbit, distance1, angle1_inc)), size1)
-    pygame.draw.circle(screen, blue, (planet_move.planet_coors(planet2_orbit, distance2, angle2_inc)), size2)
-    pygame.draw.circle(screen, red, (planet_move.planet_coors(planet3_orbit, distance3, angle3_inc)), size3)
-    pygame.draw.circle(screen, green, (planet_move.planet_coors(planet4_orbit, distance4, angle4_inc)), size4)
+    pygame.draw.circle(screen, yellow, (PlanetMove.planet_coors(planet1_orbit, distance1)), size1)
+    pygame.draw.circle(screen, blue, (PlanetMove.planet_coors(planet2_orbit, distance2)), size2)
+    pygame.draw.circle(screen, red, (PlanetMove.planet_coors(planet3_orbit, distance3)), size3)
+    pygame.draw.circle(screen, green, (PlanetMove.planet_coors(planet4_orbit, distance4)), size4)
 
-    
-    
-    
     pygame.display.flip()
 
     clock.tick(60)
