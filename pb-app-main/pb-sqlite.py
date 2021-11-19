fa4b9aec98294ea6a686b7095511d12528ea9d70
Giorgos-vr <89841637+Giorgos-vr@users.noreply.github.com>
2021-11-19 21:35:57 +0200

    testing

--- pb-app-main/pb-sqlite.py
+++ pb-app-main/pb-sqlite.py
@@ -2,6 +2,7 @@ import sqlite3 as sl
 
 con = sl.connect('pb.db')
 
+
 with con:
     con.execute ("""
     CREATE TABLE PB (
@@ -14,3 +15,13 @@ with con:
     ) ;
     """)
 
+#sql = 'INSERT INTO PB (id, name, number_primary, number_secondary, email, anniversary) values (?, ?, ?, ?, ?, ?)'
+#data = [(1, 'Bob', '001', '0001','bob@mail.com', '2021-01-01'), (2, 'Mike', '002', '0002', 'mike@mail.com', '2020-01-30')]
+
+#with con:
+    #con.executemany(sql, data)
+
+#with con:
+    #data = con.execute('SELECT * FROM pb WHERE id <= 3')
+    #for row in data:
+        #print(row)
