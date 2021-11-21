20d000dc03037ee8d95ccbadd76c764864d02c53
Giorgos-vr <89841637+Giorgos-vr@users.noreply.github.com>
2021-11-21 13:47:01 +0200

    testing

--- pb-app-main/pb-sqlite.py
+++ pb-app-main/pb-sqlite.py
@@ -1,19 +1,25 @@
 import sqlite3 as sl
+from sqlite3 import Error
+import os
 
-con = sl.connect('pb.db')
+
+file_path = os.path.join("data")
+os.makedirs(file_path, exist_ok=True)
+db = os.path.join(file_path, "pb.db")
+con = sl.connect(db)
 
 
-with con:
-    con.execute ("""
-    CREATE TABLE PB (
-        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-        name TEXT,
-        number_primary TEXT,
-        number_secondary TEXT,
-        email TEXT,
-        anniversary DATE
-    ) ;
-    """)
+#with con:
+    #con.execute ("""
+    #CREATE TABLE PB (
+        #id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        #name TEXT,
+        #number_primary TEXT,
+        #number_secondary TEXT,
+        #email TEXT,
+        #anniversary DATE
+    #) ;
+    #""")
 
 #sql = 'INSERT INTO PB (id, name, number_primary, number_secondary, email, anniversary) values (?, ?, ?, ?, ?, ?)'
 #data = [(1, 'Bob', '001', '0001','bob@mail.com', '2021-01-01'), (2, 'Mike', '002', '0002', 'mike@mail.com', '2020-01-30')]
@@ -21,7 +27,7 @@ with con:
 #with con:
     #con.executemany(sql, data)
 
-#with con:
-    #data = con.execute('SELECT * FROM pb WHERE id <= 3')
-    #for row in data:
-        #print(row)
+with con:
+    data = con.execute('SELECT * FROM pb WHERE id <= 3')
+    for row in data:
+        print(row)
