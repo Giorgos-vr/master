551f5f2e20a87c2048ab018979efc928cde7ec34
Giorgos-vr <89841637+Giorgos-vr@users.noreply.github.com>
2021-11-22 16:16:58 +0200

    graph exercise

--- school-projects-main/l2384-c1-u4_Vrachliotis_cos2pix.py
+++ school-projects-main/l2384-c1-u4_Vrachliotis_cos2pix.py
@@ -6,8 +6,8 @@ import numpy as num
 x = num.arange(0.0, 5.0, 0.1)
 y = num.cos(2*num.pi*x)
 
-plt.plot(x,y)
 plt.xlabel('arange(0.0, 5.0, 0.1)')
 plt.ylabel('2πΧ')
 plt.title('Y=cos(2πX)')
+plt.plot(x,y)
 plt.show()
