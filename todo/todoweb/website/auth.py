83dfa138e8f1bdadbb057b1aa6b99b2d23c5ddc6
Giorgos-vr <89841637+Giorgos-vr@users.noreply.github.com>
2022-01-14 17:10:18 +0200

    not even close to finished...

--- todo/todoweb/website/auth.py
+++ todo/todoweb/website/auth.py
@@ -23,7 +23,7 @@ def login():
         else:
             flash('wrong email, new user? please consider signing up first!', category='Error')
 
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
 @login_required
@@ -59,4 +59,4 @@ def sign_up():
             flash("Account created!", category="Confirm")
             return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
--- todo/todoweb/website/views.py
+++ todo/todoweb/website/views.py
@@ -1,10 +1,36 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
 
 views = Blueprint('views', __name__)
 
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'] )
 @login_required
 def home():
-    return render_template("home.html")
+    if request.method == 'POST':
+        note= request.form.get('note')
+
+        if len(note)<3:
+            flash('note too short, must be at least 3 characters long', category="Error")
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note Saved', category="Confirm")
+    return render_template("home.html", user=current_user)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+            
+    return jsonify({})
