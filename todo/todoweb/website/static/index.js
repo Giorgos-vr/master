acad01c3a81de42ec305a29d04830c9d30dc3830
Giorgos-vr <89841637+Giorgos-vr@users.noreply.github.com>
2022-01-15 13:10:40 +0200

    added new button

--- todo/todoweb/website/static/index.js
+++ todo/todoweb/website/static/index.js
@@ -6,3 +6,12 @@ function deleteNote(noteId) {
       window.location.href = "/";
     });
   }
+
+  function changeNote(noteId) {
+    fetch("/change-note", {
+      method: "POST",
+      body: JSON.stringify({ noteId: noteId }),
+    }).then((_res) => {
+      window.location.href = "/";
+    });
+  }
