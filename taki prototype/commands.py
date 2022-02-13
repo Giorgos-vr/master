3e6960a1537d08bc0b968edaf859ef959a5ca882
Giorgos-vr <89841637+Giorgos-vr@users.noreply.github.com>
2022-02-13 22:12:20 +0200

    3 hours to say ONE sentence!

---
+++ taki prototype/commands.py
@@ -0,0 +1,39 @@
+from fileinput import filename
+import os
+import time
+import playsound
+import speech_recognition as sr
+from gtts import gTTS as gt
+
+
+def say(text):
+    tts = gt(text=text, lang="el")
+    filename = "say.mp3"
+    tts.save(filename)
+    playsound.playsound(filename)
+    os.remove(filename)
+
+
+
+def audioIn():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        global user_said
+        user_said = ""
+        
+
+        try:
+            user_said = r.recognize_google(audio, language = "el-GR")
+            print(user_said)
+        except Exception as e:
+            print ("Exception: " + str(e))
+    
+    return user_said
+
+
+def start():
+    say("Γειά σου, με λένε Τίνα, από το Ρομποτίνα. Εσένα πως σε λένε;")
+    audioIn(); say(f"Γειά σου {user_said}")
+
+start()
---
+++ taki prototype/main.py
@@ -0,0 +1,15 @@
+from fileinput import filename
+from .commands import *
+import os
+import time
+import playsound
+import speech_recognition as sr
+from gtts import gTTS as gt
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
