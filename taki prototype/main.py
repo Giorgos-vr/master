b49338664c93a66389f542987dfdc1c526d2832b
Giorgos-vr <89841637+Giorgos-vr@users.noreply.github.com>
2022-02-20 22:26:54 +0200

    can't hack it today...

--- taki prototype/main.py
+++ taki prototype/main.py
@@ -1,1 +1,20 @@
 #fresh look...
+
+import time
+
+import speech_recognition as sr
+
+
+r = sr.Recognizer()                    
+keyword = "taki"                                                                
+with sr.Microphone() as source:                                                                       
+    print("Speak:")                                                                                   
+    audio = r.listen(source)   
+
+try:
+    if  r.recognize_google(audio) == keyword:
+        print("taki")
+except sr.UnknownValueError:
+    print("Could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results; {0}".format(e))
