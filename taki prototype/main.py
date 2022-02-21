5895fcffea8687edb1b8685241f6d3acd6f56a89
Giorgos-vr <89841637+Giorgos-vr@users.noreply.github.com>
2022-02-21 16:54:13 +0200

    works kinda

--- taki prototype/main.py
+++ taki prototype/main.py
@@ -1,20 +1,64 @@
-#fresh look...
-
+from fileinput import filename
+import os
 import time
-
+import playsound
 import speech_recognition as sr
+from gtts import gTTS as gt
+
+r = sr.Recognizer()
+
+# Words that sphinx should listen closely for. 0-1 is the sensitivity
+# of the wake word.
+keywords = [("tacky", 0.9), ("blue", 1), ]
+
+source = sr.Microphone()
+
+def audioIn():
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            audio = r.listen(source)
+            user_said = ""
+            
+
+            try:
+                user_said = r.recognize_google(audio, language = "el-GR")
+                print(user_said)
+            except sr.UnknownValueError:
+                say("Δεν σε κατάλαβα, συγνώμη!")
+                pass
+            except sr.RequestError:
+                say("Αδυναμία σύνδεσης!")
+                pass
+        
+        return user_said
+
+def callback(recognizer, audio):
+
+    try:
+        speech_as_text = recognizer.recognize_sphinx(audio, keyword_entries=keywords)
+        print(speech_as_text)
+
+        # Look for keyword in speech_as_text
+        if "tacky" in speech_as_text or "blue":
+            audioIn()
+
+    except sr.UnknownValueError:
+        print("Oops! Didn't catch that")
 
 
-r = sr.Recognizer()                    
-keyword = "taki"                                                                
-with sr.Microphone() as source:                                                                       
-    print("Speak:")                                                                                   
-    audio = r.listen(source)   
+def say(text):
+        tts = gt(text=text, lang="el")
+        filename = "say.mp3"
+        tts.save(filename)
+        playsound.playsound(filename)
+        os.remove(filename)
 
-try:
-    if  r.recognize_google(audio) == keyword:
-        print("taki")
-except sr.UnknownValueError:
-    print("Could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results; {0}".format(e))
+
+
+
+def start_recognizer():
+    r.listen_in_background(source, callback)
+    time.sleep(1000000)
+
+
+start_recognizer()
